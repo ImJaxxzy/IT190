fa9ec88a149175dfbc41c734c0735dba45b845f4
ImJaxxzy <christopher.hardy204@yahoo.com>
2023-04-13 22:15:10 -0700

    HW script finished, new folder for complete script

---
+++ finished/HelloWorld.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# A simple "Hello World" script in python
+# Created by Chris Hardy, 4/13/2023
+print("Hello World")
+user_name = input ("Please enter your name: ")
+print("Good to see you, "+ user_name +"!")
+age = int(input("Please enter your age: "))
+new_age = age + 2
+print("In two years, you will be ", new_age ," Years old.")
